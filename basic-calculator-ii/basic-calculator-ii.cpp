42e91a1e0e68f043dd5313f00443f91a090aeada
Nikita Saini <79187693+nikitaaa23@users.noreply.github.com>
2021-12-25 19:56:57 +0530

    Time: 12 ms (56.08%), Space: 8.8 MB (25.68%) - LeetHub

---
+++ basic-calculator-ii/basic-calculator-ii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int calculate(string s) {
+        s+='+';
+        stack<int>st;
+        int curr =0;
+        char sign = '+';
+        long long int ans = 0;
+        for(int i = 0; i<s.size(); i++){
+            if(isdigit(s[i]))
+                curr = curr*10 + (s[i]-'0');
+    
+            else if(s[i]=='+' || s[i]=='-' || s[i]=='*' || s[i]=='/'){
+                if(sign=='+'){
+                    st.push(curr);
+
+                }
+                else if(sign=='-'){
+                    st.push(curr*(-1));
+                }
+                else if(sign=='*'){
+                    int num = st.top();
+                    st.pop();
+                    st.push(num*curr);
+                }
+                else if(sign=='/'){
+                    int num = st.top();
+                    st.pop();
+                    st.push(num/curr);
+                }
+                 curr = 0;
+                sign = s[i];
+            }          
+        }
+        
+        while(!st.empty()){
+            ans +=st.top();
+            st.pop();
+        }
+        return ans;
+    }
+};
