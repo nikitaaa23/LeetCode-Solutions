aa069d5c3579b55334aa8c52c23a94d5d9ebef42
Nikita Saini <79187693+nikitaaa23@users.noreply.github.com>
2022-02-01 15:57:05 +0530

    Time: 1 ms (34.93%), Space: 45.6 MB (5.01%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.java
@@ -1,91 +1,34 @@
 /*
 // Definition for a Node.
 class Node {
-public:
     int val;
-    Node* next;
-    Node* random;
-    
-    Node(int _val) {
-        val = _val;
-        next = NULL;
-        random = NULL;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
     }
-};
+}
 */
 
 class Solution {
-public:
-    //O(n), O(n)
-   /* Node* copyRandomList(Node* head) {
-        if(head==NULL) return NULL;
-        Node* ans = head;
-        unordered_map<Node*, Node*>mp;
-        
-        Node* curr = head;
-        Node* prev = NULL;
-        Node* nHead = NULL;
-        
-        while(curr){
-            Node* n = new Node(curr->val);
-            if(!prev) nHead = n;
-            else
-                prev->next = n;
-            prev = n;
-            mp[curr] = n;
-            curr = curr->next;
-        }
-        
-        curr = head;
-        Node* tmp = nHead;
-        
-        while(curr){
-            Node* x = curr->random;
-            if(!x) tmp->random = NULL;
-            else
-                tmp->random = mp[x];
-            tmp = tmp->next;
-            curr= curr->next;
-        }
-        return nHead;
-    }*/
-    
-    
-    
-    Node* copyRandomList(Node* head) {
-        if(head==NULL) return NULL;
-        Node* iter = head;
-        Node* front = head;
-        
-        while(iter){
-            front = iter->next;
-            Node* c = new Node(iter->val);
-            iter->next = c;
-            c->next = front;
-            
-            iter = front;
-        }
-        
-        iter = head;
-        while(iter){
-            if(iter->random !=NULL){
-                iter->next->random = iter->random->next;
-            }
-            iter = iter->next->next;
-        }
-        
-        iter = head;
-        Node* p = new Node(0);
-        Node* cop = p;
-        
-        while(iter){
-            front = iter->next->next;
-            cop->next = iter->next;
-            iter->next = front;
-            cop = cop->next;
-            iter = front;
-        }
-        
-        return p->next;
+    public Node copyRandomList(Node head) {
+        HashMap<Node, Node> mp = new HashMap<>();
+        return cloneptr(head, mp);
     }
-};
+     public Node cloneptr(Node old, HashMap<Node, Node> mp){
+        if(old == null){
+            return null;
+        }
+
+        Node newNode = new Node(old.val);
+        mp.put(old, newNode);
+
+        newNode.next = cloneptr(old.next, mp);
+        newNode.random = mp.get(old.random);
+
+        return newNode;
+    }
+}
