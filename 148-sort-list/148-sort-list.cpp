77c8f15aa8eb833ce19da063a67a0bd3989647b4
Nikita Saini <79187693+nikitaaa23@users.noreply.github.com>
2022-02-25 00:14:43 +0530

    Time: 161 ms (39.32%), Space: 48.5 MB (32.24%) - LeetHub

--- 148-sort-list/148-sort-list.cpp
+++ 148-sort-list/148-sort-list.cpp
@@ -11,9 +11,36 @@
 class Solution {
 public:
     
-    ListNode* mid (ListNode* head) {
-          ListNode* slow = head, *fast = head, *n = NULL;
+    ListNode* merge(ListNode* l1, ListNode* l2){
+        if(!l1)
+            return l2;
+        if(!l2)
+            return l1;
+    
+        ListNode* ptr = new ListNode(0), *res=ptr;
         
+        while(l1 && l2){
+            if(l1->val <l2->val){
+                res->next = l1;
+                l1 = l1->next;
+            }
+            else{
+                res->next = l2;
+                l2 = l2->next;
+            }
+            res = res->next;
+        }
+        
+        if(l1)
+            res->next = l1;
+        if(l2)
+            res->next = l2;
+        
+        return ptr->next;
+    }
+    
+    ListNode* mid(ListNode* head){
+        ListNode* slow = head, *fast = head, *n = NULL;
         while(fast && fast->next){
             n = slow;
             slow = slow->next;
@@ -21,39 +48,13 @@ public:
         }
         return n;
     }
-    ListNode* merge(ListNode* l1, ListNode* l2){
-        if(!l1)
-            return l2;
-        if(!l2)
-            return l1;
-        ListNode* ptr = new ListNode(0), *res = ptr;
-        while(l1 && l2){
-            if(l1->val < l2->val){
-                res->next = l1;
-                l1 = l1->next;
-            }
-            else{
-                res->next =l2;
-                l2 = l2->next;
-            }
-            res = res->next;
-        }
-        if(l1){
-            res->next = l1;
-        }
-        else if(l2){
-            res->next = l2;
-        }
-        
-        return ptr->next;
-    }
     ListNode* sortList(ListNode* head) {
-        if(head==NULL || head->next == NULL)
+        if(head==NULL || head->next == NULL){
             return head;
-      ListNode* m = mid(head);
+        }
+        ListNode* m = mid(head);
         ListNode* n = m->next;
         m->next = NULL;
-        
         return merge(sortList(head), sortList(n));
     }
 };
