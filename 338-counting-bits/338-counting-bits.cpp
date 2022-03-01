9a48e607405c313fb9832d8be12bbf7c1470f93e
Nikita Saini <79187693+nikitaaa23@users.noreply.github.com>
2022-03-01 12:39:03 +0530

    Time: 7 ms (62.32%), Space: 7.7 MB (89.06%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -2,28 +2,28 @@ class Solution {
 public:
     vector<int> countBits(int n) {
         if(n==0) return {0};
-        vector<int>dp;
-//         dp[0] = 0;
-//         dp[1] = 1;
-//         for(int i = 2; i<=n ;i++){
-//             if(i%2==0){
-//                 dp[i] = dp[i/2];
+        vector<int>dp(n+1, 0);
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2; i<=n ;i++){
+            if(i%2==0){
+                dp[i] = dp[i/2];
                 
-//             }
-//             else
-//                 dp[i] = dp[i/2] + 1;
-//         }
-//         return dp;
-        
-        for(int i = 0; i<=n; i++){
-            int num = i;
-            int sum = 0;
-            while(num!=0){
-                sum+=num%2;
-                num = num/2;
             }
-            dp.push_back(sum);
+            else
+                dp[i] = dp[i/2] + 1;
         }
         return dp;
+        
+        // for(int i = 0; i<=n; i++){
+        //     int num = i;
+        //     int sum = 0;
+        //     while(num!=0){
+        //         sum+=num%2;
+        //         num = num/2;
+        //     }
+        //     dp.push_back(sum);
+        // }
+        // return dp;
     }
 };
