36d34776d79243802081583c2afd8552e140c782
Nikita Saini <79187693+nikitaaa23@users.noreply.github.com>
2022-03-03 12:01:03 +0530

    Time: 4 ms (49.49%), Space: 7.3 MB (55.42%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -7,17 +7,31 @@ public:
         if(n<3)
             return 0;
         int count =0, diff = 0;
-        for(int i =0 ;i<n-2; i++){
-            diff = nums[i+1] - nums[i];
-            for(int j = i+2; j<n; j++){
-                if(nums[j] - nums[j-1] == diff){
-                    count++;
-                }
-                else
-                    break;
-            }
-        }
+//         for(int i =0 ;i<n-2; i++){
+//             diff = nums[i+1] - nums[i];
+//             for(int j = i+2; j<n; j++){
+//                 if(nums[j] - nums[j-1] == diff){
+//                     count++;
+//                 }
+//                 else
+//                     break;
+//             }
+//         }
         
+//         return count;
+        int prev = nums[1] - nums[0], cnt = 0;
+        
+        for(int i  = 1; i<n-1; i++){
+            diff = nums[i+1] - nums[i];
+            
+            if(diff==prev)
+                cnt++;
+            else{
+                prev = diff;
+                cnt = 0;
+            }
+            count+=cnt;
+        }
         return count;
     }
 };
