ca15f48eb6e1bf42c1a781fe7fa86a0baa2f9d9a
Nikita Saini <79187693+nikitaaa23@users.noreply.github.com>
2022-03-04 13:30:45 +0530

    Time: 68 ms (86.68%), Space: 10.6 MB (82.88%) - LeetHub

---
+++ 322-coin-change/322-coin-change.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    #define INF INT_MAX-1
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        int t[n+1][amount+1];
+        
+        for(int i=0; i<=n; i++)
+        {
+            for(int j=0; j<=amount; j++)
+            {
+                if(i==0){ t[i][j] = INF ;}
+                if(j==0){ t[i][j] = 0 ;}
+                if(i==1)
+                {   
+                    if((j%coins[i-1]) == 0){ t[i][j] = j/coins[i-1] ;}
+                    else{ t[i][j] = INF ;}
+                }
+            }
+        }
+        
+        
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=amount; j++){
+                if(coins[i-1]<=j){
+                    t[i][j] = min(t[i-1][j], 1+t[i][j-coins[i-1]]);
+                }
+                else
+                    t[i][j] = t[i-1][j];
+            }
+        }
+        
+        return t[n][amount] > amount ? -1 : t[n][amount];
+    }
+};
