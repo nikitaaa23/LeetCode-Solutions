b41e2deeb2757cbff1a8ffe191750e885cc0ec68
Nikita Saini <79187693+nikitaaa23@users.noreply.github.com>
2022-03-06 15:37:10 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (100.00%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -1,32 +1,15 @@
 class Solution {
 public:
-    int mod = 1e9+7;
-    vector<vector<int>>dp;
-    
-    long total(int unp, int und){
-        if(unp==0 && und==0){
-            return 1;
-        }
-        
-        if(unp<0 || und<0 || und<unp){
-            return 0;
-        }
-        
-        
-        if(dp[unp][und]){
-            return dp[unp][und];
-        }
-        long ans =0;
-        ans+=unp * total(unp-1, und);
-        ans%=mod;
-        ans+= (und-unp ) * total(unp, und-1);
-        ans%=mod;
-        
-        return dp[unp][und] = ans;
-    } 
-    
     int countOrders(int n) {
-        dp = vector<vector<int>>(n+1, vector<int>(n+1, 0));
-        return total(n, n);
+         long res = 1, mod = 1000000007;
+        for(int i = 1; i <=n; ++i){			
+            res *= i;
+            res %= mod;
+			
+            res *= (2*i-1);
+            res %= mod;
+        }
+   
+        return res % mod;
     }
 };
