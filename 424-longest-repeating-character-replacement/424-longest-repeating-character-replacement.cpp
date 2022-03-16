7303651874025904d69cc018f60313aca101f9af
Nikita Saini <79187693+nikitaaa23@users.noreply.github.com>
2022-03-16 13:26:36 +0530

    Time: 25 ms (31.11%), Space: 7.1 MB (51.97%) - LeetHub

---
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        unordered_map<char, int>mp;
+        int i =0, j=0;
+        int len = 0;
+        int n = s.size();
+        while(j<n){
+            int mx = 0;
+            mp[s[j]]++;
+            for(auto x : mp){
+                mx = max(mx, x.second);
+            }
+            if(j-i+1 - mx <= k){
+                len = max(len, j-i+1);
+            }
+            else if(j - i+1 -mx >k){
+                mp[s[i]]--;
+                i++;
+            }
+            j++;
+        }
+        return len;
+    }
+};
