92793db59b1aa7e2e3dbb7722de3182f6731d7bc
Nikita Saini <79187693+nikitaaa23@users.noreply.github.com>
2022-03-17 00:42:52 +0530

    Time: 38 ms (97.75%), Space: 9.9 MB (99.42%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -3,33 +3,47 @@ public:
     #define INF INT_MAX-1
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        int t[n+1][amount+1];
+        int t[amount+1];
         
-        for(int i=0; i<=n; i++)
-        {
-            for(int j=0; j<=amount; j++)
-            {
-                if(i==0){ t[i][j] = INF ;}
-                if(j==0){ t[i][j] = 0 ;}
-                if(i==1)
-                {   
-                    if((j%coins[i-1]) == 0){ t[i][j] = j/coins[i-1] ;}
-                    else{ t[i][j] = INF ;}
+//         for(int i=0; i<=n; i++)
+//         {
+//             for(int j=0; j<=amount; j++)
+//             {
+//                 if(i==0){ t[i][j] = INF ;}
+//                 if(j==0){ t[i][j] = 0 ;}
+//                 if(i==1)
+//                 {   
+//                     if((j%coins[i-1]) == 0){ t[i][j] = j/coins[i-1] ;}
+//                     else{ t[i][j] = INF ;}
+//                 }
+//             }
+//         }
+        
+        
+//         for(int i = 1; i<=n; i++){
+//             for(int j = 1; j<=amount; j++){
+//                 if(coins[i-1]<=j){
+//                     t[i][j] = min(t[i-1][j], 1+t[i][j-coins[i-1]]);
+//                 }
+//                 else
+//                     t[i][j] = t[i-1][j];
+//             }
+//         }
+        
+//         return t[n][amount] > amount ? -1 : t[n][amount];
+        
+        for(int i = 0; i<amount+1; i++){
+            t[i] = INT_MAX-1;
+        }
+        t[0] = 0;
+        
+        for(int i =1; i<amount+1; i++){
+            for(auto c : coins){
+                if(i - c >= 0){
+                    t[i] = min(t[i], 1+t[i-c]);
                 }
             }
         }
-        
-        
-        for(int i = 1; i<=n; i++){
-            for(int j = 1; j<=amount; j++){
-                if(coins[i-1]<=j){
-                    t[i][j] = min(t[i-1][j], 1+t[i][j-coins[i-1]]);
-                }
-                else
-                    t[i][j] = t[i-1][j];
-            }
-        }
-        
-        return t[n][amount] > amount ? -1 : t[n][amount];
+        return t[amount] == INT_MAX-1 ? -1 : t[amount];
     }
 };
