0409203e4ad757ead32bd2822c438af2b9f584f7
Nikita Saini <79187693+nikitaaa23@users.noreply.github.com>
2022-04-24 21:13:15 +0530

    Time: 28 ms (25.38%), Space: 7 MB (52.26%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -5,16 +5,32 @@ public:
         int i =0, j=0;
         int len = 0;
         int n = s.size();
+        // while(j<n){
+        //     int mx = 0;
+        //     mp[s[j]]++;
+        //     for(auto x : mp){
+        //         mx = max(mx, x.second);
+        //     }
+        //     if(j-i+1 - mx <= k){
+        //         len = max(len, j-i+1);
+        //     }
+        //     else if(j - i+1 -mx >k){
+        //         mp[s[i]]--;
+        //         i++;
+        //     }
+        //     j++;
+        // }
+        
         while(j<n){
             int mx = 0;
             mp[s[j]]++;
             for(auto x : mp){
                 mx = max(mx, x.second);
             }
-            if(j-i+1 - mx <= k){
+            if(j-i+1 - mx <=k ){
                 len = max(len, j-i+1);
             }
-            else if(j - i+1 -mx >k){
+            else if(j-i+1 -mx > k){
                 mp[s[i]]--;
                 i++;
             }
