28f85c1e8f6556b3ea752a2adc89132859d84aff
Nikita Saini <79187693+nikitaaa23@users.noreply.github.com>
2022-04-25 22:42:14 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (92.55%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -7,35 +7,33 @@ public:
         int left = 0, right= m-1;
         int dir = 0;
         vector<int>v;
-        
-        while(top<=down && left<=right){
-            if(dir == 0){
-                for(int i = left; i<=right;i++){
-                    v.push_back(matrix[top][i]);
-                }
-                top++;
-            }
-            else if(dir==1){
-                for(int i = top; i<=down; i++){
-                    v.push_back(matrix[i][right]);
-                }
-                right--;
-            }
-            else if(dir==2){
-                for(int i = right; i>=left; i--){
-                    v.push_back(matrix[down][i]);
-                }
-                down--;
-            }
-            else if(dir==3){
-                for(int i = down; i>=top; i--){
-                    v.push_back(matrix[i][left]);
-                }
-                left++;
-            }
-            dir=(dir+1)%4;
-        }
+       while(top<=down && left<=right){
+           if(dir==0){
+               for(int i=left; i<=right; i++){
+                   v.push_back(matrix[top][i]);
+               }
+               top++;
+           }
+           else if(dir==1){
+               for(int i=top; i<=down; i++){
+                   v.push_back(matrix[i][right]);
+               }
+               right--;
+           }
+           else if(dir==2){
+               for(int i=right; i>=left; i--){
+                   v.push_back(matrix[down][i]);
+               }
+               down--;
+           }
+           else{
+               for(int i=down; i>=top; i--){
+                   v.push_back(matrix[i][left]);
+               }
+               left++;
+           }
+           dir = (dir+1)%4;
+       }
         return v;
-        
     }
 };
