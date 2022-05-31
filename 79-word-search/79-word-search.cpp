9d5ab1c0c7d1ee7ba83debfe10f852b09b249616
Nikita Saini <79187693+nikitaaa23@users.noreply.github.com>
2022-06-01 00:06:36 +0530

    Time: 416 ms (60.85%), Space: 8.1 MB (36.96%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,43 +1,36 @@
 class Solution {
 public:
-    bool backtrack(int i, int j, vector<vector<int>>&vis, vector<vector<char>> &board, string &word, int n, int m, int idx){
-
-          
- if(idx==word.size()){
-          return true;
-      }
-      
-     if(i<0 || i>=board.size() || j<0 || j>=board[0].size() || vis[i][j] == true || board[i][j]!=word[idx]){
+    
+    bool dfs(int r, int c, vector<vector<char>>& board, vector<vector<int>>&vis, string &word, int idx){
+        if(idx == word.size())
+            return true;
+       if(r >= board.size() || r<0 || c<0 || c>=board[0].size() || vis[r][c]==true ||
+         word[idx]!=board[r][c])
            return false;
-       }
- 
-      if(board[i][j] == word[idx]){
-          vis[i][j] = true;
-          bool ispos = (backtrack(i+1, j, vis, board, word, n, m, idx+1)
-            ||  backtrack(i-1, j, vis, board, word, n, m, idx+1)
-            ||  backtrack(i, j+1, vis, board, word, n, m, idx+1)
-            ||  backtrack(i, j-1, vis, board, word, n, m, idx+1)) ; 
-            vis[i][j] = false;
-            return ispos;
-      }
-      
-          return false;
-
-  }
+        if(word[idx] == board[r][c]){
+            vis[r][c] = true;
+        bool res = dfs(r+1, c, board, vis, word, idx+1)
+            ||  dfs(r-1, c, board, vis, word, idx+1)
+            ||  dfs(r, c+1, board, vis, word, idx+1)
+            ||  dfs(r, c-1, board, vis, word, idx+1);
+        vis[r][c] = false;
+        return res;
+        }
+        return false;
+    }
+    
     bool exist(vector<vector<char>>& board, string word) {
-        int n = board.size();
-        int m = board[0].size();
-        vector<vector<int>>vis(n, vector<int>(m, 0));
-        for(int i= 0; i<board.size(); i++){
+         vector<vector<int>>vis(board.size(), vector<int>(board[0].size(), 0));
+        for(int i = 0;i<board.size(); i++){
             for(int j = 0; j<board[0].size(); j++){
-                if(board[i][j] == word[0]){
-                    if(backtrack(i, j, vis, board, word, n, m, 0)){
+                if(board[i][j]==word[0]){
+                    if(dfs(i, j, board, vis, word, 0)){
                         return true;
                     }
-                        
                 }
             }
         }
+        
         return false;
     }
 };
